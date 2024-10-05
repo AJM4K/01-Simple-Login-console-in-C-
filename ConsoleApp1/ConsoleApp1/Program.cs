be0a4f5979cd5f824fd0a26aa84d9ef29a3e93d5
AJM4K <ahmedmahdi.4k@gmail.com>
2024-10-05 16:33:29 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void CheckOTP ()
+        {
+            Random random = new Random();
+            int generatedOTP = random.Next(100000, 999999);
+            Console.WriteLine($"This is the OTP Code {generatedOTP}");
+            Console.WriteLine("Enter the OTP code: ");
+            string inputOTP = Console.ReadLine();
+            int userOTP = 0;
+            bool isInputOTPIsNumber = int.TryParse(inputOTP, out userOTP);
+
+            if (!isInputOTPIsNumber)
+            {
+                Console.WriteLine("invalid input OTP");
+                Environment.Exit(0);
+            }
+
+            if (userOTP != generatedOTP)
+            {
+                Console.WriteLine("invalid OTP");
+                Environment.Exit(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            const string USERNAME = "username";
+            const string PASSWORD = "password";
+            Console.WriteLine("welcome to the simple login system!");
+            
+            Console.WriteLine("Enter username:");
+            string inputUser = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string inputPassword = Console.ReadLine();
+            
+            bool isValidInputs = inputUser == USERNAME && inputPassword == PASSWORD;
+            if (!isValidInputs ) {
+                Console.WriteLine("invalid credentials");
+                Environment.Exit(0);
+            }
+
+            CheckOTP();
+           
+            
+            Console.WriteLine("you are logged in");
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Simple login Console application
+
+This is the first task that was assigned by AON 2 Backend Training.
+
+Done by: Ahmed Mahdi ( @AJM4K )
+email: AhmedMahdi.4k@gmail.com
+phone: +9647838964777
+Date: 6/10/2024 
+
+### Below are screenshoots that show case different scenarioes
+
+##### valid login 
+
+![1728134693464](image/readme/1728134693464.png)
+
+##### invalid username or password 
+
+![1728134932437](image/readme/1728134932437.png)
+
+##### Invalid OTP code 
+
+![1728134945908](image/readme/1728134945908.png)
+
+##### Invalid input in the OTP code 
+
+![1728134954599](image/readme/1728134954599.png)
